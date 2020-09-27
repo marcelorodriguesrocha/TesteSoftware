1c6eb10a2ca31267e14841f576089f32a56553bb
Marcelo Rocha <mrodriguesrocha@gmail.com>
2020-09-27 19:46:30 -0300

    Passando o Mouse - Hover

---
+++ spec/hovers_spec.rb
@@ -0,0 +1,37 @@
+# houver -> Quando voce passa o mouse por cima da imagem aparece o nome.
+
+describe 'Mouse Hover', :hovers do
+    
+    before(:each)do
+        visit 'https://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'Quando passo o mouse no Blade' do
+        card = find ('img[alt=Blade]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end
+
+    it 'Quando passo o mouse sobre o Pantera Negra' do
+        card = find ('img[alt="Pantera Negra"]') # use "" para marcar como string e porque tem espaço entre palavras
+        card.hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end
+
+    it 'Quando passo o mouse sobre o Homem Aranha' do
+        card = find ('img[alt="Homem Aranha"]')
+        card.hover 
+
+        expect(page).to have_content 'Nome: Homem Aranha'
+    end
+
+
+
+
+    after(:each)do
+        sleep 0.5
+    end
+
+end
