f83c4c48edce4fce1947cf1378a9f7f47ff3432a
Marcelo Rocha <mrodriguesrocha@gmail.com>
2020-10-06 11:11:56 -0300

    Iframes bom/ruim

---
+++ spec/iframes_spec.rb
@@ -0,0 +1,45 @@
+### Iframe => Cria um html dentro do outro ###
+### Vamos adcionar um produto no carrinho ####
+
+describe 'iframes' do
+    
+    describe 'bom ', :nice_iframe do
+        
+        before(:each) do
+            visit 'https://training-wheels-protocol.herokuapp.com/nice_iframe'
+        end
+
+        it 'Adicionar ao carrinho' do
+            
+            within_frame('burgerId') do #with -> quer dizer escopo
+                produto = find('.menu-item-info-box', text: 'REFRIGERANTE') # sempre que vou procurar tenho que colocar o . antes
+                
+                produto.find('a').click  # a -> porque ele é um link no html 
+                
+                expect(find('#cart')).to have_content 'R$ 4,50'
+                sleep 5
+            end  
+        end
+
+    end
+
+    describe 'Ruim', :bad_iframe do
+        before(:each) do
+            visit 'https://training-wheels-protocol.herokuapp.com/bad_iframe'
+        end
+
+        it 'Carrinho deve estar vazio' do
+            
+            script = '$(".box-iframe").attr("id", "tempId")'; # substitui ' por " para transformar em string e depis coloquei a ' 
+            page.execute_script(script) # vai executar o script com o id tempID
+            
+            within_frame('tempId') do # agora chama o tempId
+                expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+                sleep 3 
+            end
+            
+        end
+
+    end
+
+end
